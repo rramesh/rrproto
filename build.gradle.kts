c7202dc5ba0071074ae036d66fc061cc3ebc58b7
Ramesh Rajamani <ramesh.rajamani@gmail.com>
2019-11-21 15:34:28 +0530

    Modified build to include ability to publish package

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,8 @@
 import com.google.protobuf.gradle.*
+import org.gradle.api.tasks.bundling.Jar
+
+group = "com.rr.proto"
+version = "1.0.0"
 
 plugins {
     // Apply the Kotlin JVM plugin to add support for Kotlin.
@@ -7,8 +11,10 @@ plugins {
     id("org.jetbrains.kotlin.kapt") version "1.3.50"
     // Protobuf
     id("com.google.protobuf") version "0.8.10"
+
     java
     application
+    `maven-publish`
 }
 
 repositories{
@@ -65,3 +71,31 @@ val sourcesJar by tasks.registering(Jar::class) {
     archiveClassifier.set("sources")
     from(sourceSets.main.get().allSource)
 }
+
+publishing {
+    repositories {
+        maven {
+            name = "rrproto"
+            url = uri("https://maven.pkg.github.com/rramesh/rrproto")
+            credentials{
+                username = project.findProperty("proto.gh.user") as String? ?: System.getenv("PROTO_GH_USERNAME")
+                password = project.findProperty("proto.gh.key") as String? ?: System.getenv("PROTO_GH_KEY")
+            }
+        }
+    }
+    publications {
+        register("rrproto", MavenPublication::class) {
+            from(components["java"])
+            artifact(sourcesJar.get())
+        }
+    }
+}
+
+tasks.clean {
+    delete(
+            "${rootDir}/src/main/grpc",
+            "${rootDir}/src/main/grpckotlin",
+            "${rootDir}/src/main/java"
+    )
+}
+
